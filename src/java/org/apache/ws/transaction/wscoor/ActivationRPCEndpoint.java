a78f50c4503b4f5d6568cc282da263d6ba80f9bb
Dasarath Weeratunge <dasarath@apache.org>
2005-02-15 12:31:04 +0000

    PR: Obtained from: Submitted by: Reviewed by: CVS: ---------------------------------------------------------------------- CVS: PR: CVS:   If this change addresses a PR in the problem report tracking CVS:   database, then enter the PR number(s) here. CVS: Obtained from: CVS:   If this change has been taken from another system, such as NCSA, CVS:   then name the system in this line, otherwise delete it. CVS: Submitted by: CVS:   If this code has been contributed to Apache by someone else; i.e., CVS:   they sent us a patch or a new module, then include their name/email CVS:   address here. If this is your work then delete this line. CVS: Reviewed by: CVS:   If we are doing pre-commit code reviews and someone else has CVS:   reviewed your changes, include their name(s) here. CVS:   If you have not had it reviewed then delete this line.
    git-svn-id: https://svn.apache.org/repos/asf/webservices/kandula/trunk@233525 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/ws/transaction/wscoor/ActivationRPCEndpoint.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright  2004 The Apache Software Foundation.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.apache.ws.transaction.wscoor;
+
+import java.rmi.RemoteException;
+
+import org.apache.axis.message.addressing.PortType;
+import org.apache.ws.transaction.coordinator.CoordinationService;
+import org.apache.ws.transaction.coordinator.Coordinator;
+
+
+public class ActivationRPCEndpoint implements ActivationPortTypeRPC {
+	public static PortType PORT_TYPE= new PortType("http://schemas.xmlsoap.org/ws/2003/09/wscoor", "ActivationPortTypeRPC");
+	
+	public CreateCoordinationContextResponseType createCoordinationContextOperation(CreateCoordinationContextType parameters)
+		throws RemoteException {
+		try {
+			String coordinationType= parameters.getCoordinationType().toString();
+			Coordinator coordinator= CoordinationService.getInstance().coordinate(coordinationType);
+			CreateCoordinationContextResponseType response= new CreateCoordinationContextResponseType();
+			response.setCoordinationContext(
+				coordinator.getCoordinationContext().toCoordinationContextType());
+			return response;
+		}
+		catch (Exception e) {
+			if (e instanceof RemoteException)
+				throw (RemoteException)e;
+			else
+				throw new RemoteException(e.toString());
+		}
+	}
+}
---
+++ src/java/org/apache/ws/transaction/wscoor/ActivationRPCPort.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright  2004 The Apache Software Foundation.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.apache.ws.transaction.wscoor;
+
+import java.net.URL;
+import java.rmi.RemoteException;
+
+import org.apache.axis.types.URI;
+import org.apache.ws.transaction.utility.Service;
+
+/**
+ * @author Dasarath Weeratunge
+ * 
+ * Created on Jun 14, 2004
+ */
+public class ActivationRPCPort {
+	ActivationPortTypeRPC stub;
+
+	public ActivationRPCPort(org.apache.axis.message.addressing.EndpointReference epr) {
+		try {
+			stub= new ActivationPortTypeRPCBindingStub(new URL(epr.getAddress().toString()), new Service(epr));
+		}
+		catch (Exception e) {
+			throw new IllegalArgumentException(e.toString());
+		}
+	}
+
+	public CoordinationContext createCoordinationContext(String coordinationType) throws RemoteException {
+		try {
+			CreateCoordinationContextType parameters= new CreateCoordinationContextType();
+			parameters.setCoordinationType(new URI(coordinationType));
+			CreateCoordinationContextResponseType response= stub.createCoordinationContextOperation(parameters);
+			return new CoordinationContext(response.getCoordinationContext());
+		}
+		catch (URI.MalformedURIException e) {
+			throw new IllegalArgumentException(e + "???" + coordinationType);
+		}
+	}
+}
