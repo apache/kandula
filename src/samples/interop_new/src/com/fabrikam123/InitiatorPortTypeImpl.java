ede46b7aebeb88c78914e7b33056fcface2dec10
Nanayakkara Kuruppuge Thilena Gunarathne <thilina@apache.org>
2005-06-06 10:59:25 +0000

    This contains some new interop test cases & a small bug fix to the samples/interop/build.xml
    git-svn-id: https://svn.apache.org/repos/asf/webservices/kandula/trunk@233531 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/samples/interop_new/src/com/fabrikam123/InitiatorPortTypeImpl.java
@@ -0,0 +1,26 @@
+/*
+ * Created on May 26, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package com.fabrikam123;
+import java.rmi.RemoteException;
+
+/**
+ * @author root
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class InitiatorPortTypeImpl implements InitiatorPortType {
+
+	/* (non-Javadoc)
+	 * @see com.fabrikam123.InitiatorPortType#response()
+	 */
+	public void response() throws RemoteException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
