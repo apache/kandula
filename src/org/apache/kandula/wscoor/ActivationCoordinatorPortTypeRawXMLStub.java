33eba9ffa568c1273b34bb8382f6ba139b0f2d58
Nanayakkara Kuruppuge Thilena Gunarathne <thilina@apache.org>
2005-09-01 05:13:16 +0000

    Switching to RAW XML stuff ,without data Binding... Life is bit more easy with OM than data Binding :D
    git-svn-id: https://svn.apache.org/repos/asf/webservices/kandula/trunk/java@265653 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/apache/kandula/wscoor/ActivationCoordinatorPortTypeRawXMLStub.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright  2004 The Apache Software Foundation.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.apache.kandula.wscoor;
+
+import java.io.IOException;
+
+import javax.xml.namespace.QName;
+
+import org.apache.axis2.addressing.AnyContentType;
+import org.apache.axis2.addressing.EndpointReference;
+import org.apache.axis2.clientapi.MessageSender;
+import org.apache.axis2.context.ConfigurationContext;
+import org.apache.axis2.description.OperationDescription;
+import org.apache.axis2.description.ParameterImpl;
+import org.apache.axis2.description.ServiceDescription;
+import org.apache.axis2.description.TransportInDescription;
+import org.apache.axis2.om.OMAbstractFactory;
+import org.apache.axis2.om.OMElement;
+import org.apache.axis2.om.OMNamespace;
+import org.apache.axis2.om.impl.MIMEOutputUtils;
+import org.apache.axis2.receivers.AbstractMessageReceiver;
+import org.apache.axis2.receivers.RawXMLINOnlyMessageReceiver;
+import org.apache.axis2.transport.http.SimpleHTTPServer;
+import org.apache.axis2.util.Utils;
+import org.apache.kandula.Constants;
+
+/**
+ * @author <a href="mailto:thilina@opensource.lk"> Thilina Gunarathne </a>
+ */
+
+public class ActivationCoordinatorPortTypeRawXMLStub extends
+        org.apache.axis2.clientapi.Stub {
+    
+    private ConfigurationContext responseConfigurationContext ;
+    public static final String AXIS2_HOME = ".";
+
+    protected static org.apache.axis2.description.OperationDescription[] operations;
+
+    static {
+
+        //creating the Service
+        _service = new org.apache.axis2.description.ServiceDescription(
+                new javax.xml.namespace.QName(
+                        "http://schemas.xmlsoap.org/ws/2003/09/wscoor",
+                        "ActivationCoordinatorPortType"));
+
+        //creating the operations
+        org.apache.axis2.description.OperationDescription operationDesc;
+        operations = new org.apache.axis2.description.OperationDescription[1];
+
+        operationDesc = new org.apache.axis2.description.OperationDescription();
+        operationDesc.setName(new javax.xml.namespace.QName(
+                "http://schemas.xmlsoap.org/ws/2003/09/wscoor",
+                "CreateCoordinationContextOperation"));
+        operations[0] = operationDesc;
+        _service.addOperation(operationDesc);
+
+    }
+
+    /**
+     * Constructor
+     */
+    public ActivationCoordinatorPortTypeRawXMLStub(String axis2Home,
+            EndpointReference targetEndpoint) throws java.lang.Exception {
+        this.toEPR = targetEndpoint;
+        //creating the configuration
+        _configurationContext = new org.apache.axis2.context.ConfigurationContextFactory()
+                .buildClientConfigurationContext(axis2Home);
+        responseConfigurationContext = new org.apache.axis2.context.ConfigurationContextFactory()
+        .buildClientConfigurationContext(axis2Home);
+        _configurationContext.getAxisConfiguration().addService(_service);
+        _serviceContext = _configurationContext.createServiceContext(_service
+                .getName());
+
+    }
+
+   
+    private org.apache.axis2.soap.SOAPEnvelope createSOAPEnvelope(
+            String coordinationType) {
+        org.apache.axis2.soap.SOAPEnvelope env = super.createEnvelope();
+        org.apache.axis2.soap.SOAPFactory factory = OMAbstractFactory
+                .getSOAP12Factory();
+        OMNamespace wsCoor = factory.createOMNamespace(Constants.WS_COOR,
+                "wscoor");
+        OMElement request = factory.createOMElement(
+                "CreateCoordinationContext", wsCoor);
+        OMElement coorType = factory
+                .createOMElement("CoordinationType", wsCoor);
+        coorType.setText(coordinationType);
+        request.addChild(coorType);
+        env.getBody().addChild(request);
+        return env;
+    }
+
+    public void createCoordinationContextOperation(
+            String coordinationType, String id)
+            throws java.rmi.RemoteException {
+
+        QName serviceName = new QName("ActivationRequesterPortType");
+        QName operationName = new QName(Constants.WS_COOR,"CreateCoordinationContextOperation");
+
+        ServiceDescription service = new ServiceDescription(serviceName);
+        service.addParameter(new ParameterImpl(
+                AbstractMessageReceiver.SERVICE_CLASS,
+                ActivationRequesterPortTypeRawXMLSkeleton.class.getName()));
+        service.setFileName(ActivationRequesterPortTypeRawXMLSkeleton.class.getName());
+        org.apache.axis2.description.OperationDescription responseOperationDesc;
+        operations = new org.apache.axis2.description.OperationDescription[1];
+
+        responseOperationDesc = new org.apache.axis2.description.OperationDescription();
+        responseOperationDesc.setName(new javax.xml.namespace.QName(
+                "http://schemas.xmlsoap.org/ws/2003/09/wscoor",
+                "CreateCoordinationContextOperation"));
+        responseOperationDesc.setMessageReceiver(new RawXMLINOnlyMessageReceiver());
+        operations[0] = responseOperationDesc;
+        service.addOperation(responseOperationDesc);
+         service.setClassLoader(Thread.currentThread().getContextClassLoader());
+       
+        try {
+            SimpleHTTPServer receiver=null;
+         
+            responseConfigurationContext.getAxisConfiguration().addService(service);
+            responseConfigurationContext.createServiceContext(serviceName);
+            
+            receiver = new SimpleHTTPServer(responseConfigurationContext, 6060);
+
+            receiver.start();
+            System.out.print("Server started on port " + 5050 + ".....");
+            
+            
+            Utils.resolvePhases(receiver.getSystemContext()
+                    .getAxisConfiguration(), service);
+            MessageSender messageSender = new MessageSender(_serviceContext);
+            org.apache.axis2.context.MessageContext messageContext = getMessageContext();
+            EndpointReference replyToEpr = new EndpointReference(
+                    "http://127.0.0.1:6061" + "/axis2/services/" + serviceName.getLocalPart());
+            AnyContentType refProperties = new AnyContentType();
+            refProperties.addReferenceValue(new QName("http://ws.apache.org/kandula",id),id);
+            replyToEpr.setReferenceProperties(refProperties);
+          //  messageSender.
+            messageSender.setReplyTo(replyToEpr);
+            messageSender.setTo(this.toEPR);
+            messageSender.setSoapAction("CreateCoordinationContextOperation");
+            //_call.setWsaAction("CreateCoordinationContextOperation");
+            org.apache.axis2.soap.SOAPEnvelope env = createSOAPEnvelope(coordinationType);
+            messageContext.setEnvelope(env);
+            messageSender
+                    .setSenderTransport(org.apache.axis2.Constants.TRANSPORT_HTTP);
+            messageSender.send(operations[0], messageContext);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+
+        }
+    }
+}
---
+++ src/org/apache/kandula/wscoor/ActivationPortTypeRawXMLSkeleton.java
@@ -0,0 +1,84 @@
+package org.apache.kandula.wscoor;
+
+import javax.xml.namespace.QName;
+
+import org.apache.axis2.om.OMElement;
+import org.apache.kandula.KandulaException;
+import org.apache.kandula.coordinator.Coordinator;
+import org.apache.kandula.coordinator.CoordinatorImpl;
+import org.apache.kandula.coordinator.context.ActivityContext;
+
+/**
+ *  Auto generated java skeleton for the service by the Axis code generator
+ */
+
+public class ActivationPortTypeRawXMLSkeleton {
+
+    /**
+     * Auto generated method signature
+     *@param requestDoc
+     * @throws KandulaException
+     */
+    public OMElement CreateCoordinationContextOperation(
+            OMElement requestElement)
+            throws KandulaException {
+        ActivityContext context;
+        Coordinator coordinator = new CoordinatorImpl();
+        
+//          ActivityContext context;
+//        Coordinator coordinator = new CoordinatorImpl();
+//        if (requestDoc.getCreateCoordinationContext().getCurrentContext() != null) {
+//            CoordinationContext coorContext = CoordinationContext.Factory.newInstance(requestDoc
+//                    .getCreateCoordinationContext().getCurrentContext());
+//            context = coordinator.createCoordinationContext(coorContext);
+//        }else
+//        {
+        String coordinationType = requestElement.getFirstChildWithName(new QName("CoordinationType")).getText();
+        OMElement expiresElement = requestElement.getFirstChildWithName(new QName("Expires"));
+        String expires=null;
+        long expiresL = 0;
+        if (expiresElement!=null)
+        {
+            expires = expiresElement.getText();
+            if ((expires!=null)&&(expires.equals("")))
+            {
+                expiresL = Long.parseLong(expires);
+            }  
+        }      
+        context  = coordinator.createCoordinationContext(coordinationType,expiresL);
+        return context.getCoordinationContext().toOM();
+            
+        // context(coordinationType, expiresL);
+       // }
+//        CreateCoordinationContextResponseDocument responseDoc = CreateCoordinationContextResponseDocument.Factory
+//                .newInstance();
+//        CreateCoordinationContextResponseType responseType = CreateCoordinationContextResponseType.Factory
+//                .newInstance();
+//        responseType.setCoordinationContext((CoordinationContextType)context.getCoordinationContext().getCoordinationContextType());
+//        responseDoc.setCreateCoordinationContextResponse(responseType);
+           // return new OMElementImpl("Thilina",new OMNamespaceImpl("pre","http://www.thilina.org"));
+        
+         
+//        if (requestDoc.getCreateCoordinationContext().getCurrentContext() != null) {
+//            CoordinationContext coorContext = CoordinationContext.Factory.newInstance(requestDoc
+//                    .getCreateCoordinationContext().getCurrentContext());
+//            context = coordinator.createCoordinationContext(coorContext);
+//        }else
+//        {
+//            context = coordinator.createCoordinationContext(requestDoc
+//                    .getCreateCoordinationContext().getCoordinationType(), Long
+//                    .parseLong(requestDoc.getCreateCoordinationContext()
+//                            .getExpires().getId()));
+//        }
+//        CreateCoordinationContextResponseDocument responseDoc = CreateCoordinationContextResponseDocument.Factory
+//                .newInstance();
+//        CreateCoordinationContextResponseType responseType = CreateCoordinationContextResponseType.Factory
+//                .newInstance();
+//        responseType.setCoordinationContext((CoordinationContextType)context.getCoordinationContext().getCoordinationContextType());
+//        responseDoc.setCreateCoordinationContextResponse(responseType);
+//        return responseDoc;
+
+
+    }
+
+}
