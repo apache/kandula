8ee3975ee227951718d7ae0e59a7407440ea2b93
Dasarath Weeratunge <dasarath@apache.org>
2005-12-22 02:59:39 +0000

    git-svn-id: https://svn.apache.org/repos/asf/webservices/kandula/branches/Kandula_1@358456 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/ws/transaction/participant/j2ee/ImportedTransaction.java
+++ src/java/org/apache/ws/transaction/participant/j2ee/ImportedTransaction.java
@@ -1,18 +1,18 @@
 /*
- * Copyright  2004 The Apache Software Foundation.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
+ * Copyright 2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *  
  */
 package org.apache.ws.transaction.participant.j2ee;
 
@@ -30,36 +30,38 @@ import org.apache.ws.transaction.wsat.Vote;
 import org.apache.ws.transaction.wscoor._CoordinationContext;
 
 public class ImportedTransaction extends InterposedTransaction {
-	static final String DURABLE_2PC_PROTOCOL= "http://schemas.xmlsoap.org/ws/2004/10/wsat/Durable2PC";
-	static XATerminator xaTerminator=
-		TransactionManagerGlueFactory.getInstance().getTransactionManagerGlue().getXATerminator();
+	static final String DURABLE_2PC_PROTOCOL = "http://schemas.xmlsoap.org/ws/2004/10/wsat/Durable2PC";
+
+	static XATerminator xaTerminator = TransactionManagerGlueFactory
+			.getInstance().getTransactionManagerGlue().getXATerminator();
+
 	EndpointReference coordinator;
-	Xid xid= new XidImpl();
+
+	Xid xid = new XidImpl();
 
 	public ImportedTransaction(_CoordinationContext ctx) {
-		super.ctx= ctx;
-		System.out.println("[ImportedTransaction]"+ctx.toCoordinationContext().getRegistrationService().getAddress().getHost());
+		super.ctx = ctx;
+		System.out.println("[ImportedTransaction]"
+				+ ctx.toCoordinationContext().getRegistrationService()
+						.getAddress().getHost());
 		try {
-			coordinator=
-				ctx.register(
-					DURABLE_2PC_PROTOCOL,
-					EndpointReferenceFactory.getInstance().getEndpointReference(
-						ParticipantRPCEndpoint.PORT_TYPE,
-						ctx.getActivityId().toReferencePropertiesType()));
-			TransactionImporter importer=
-				TransactionManagerGlueFactory
-					.getInstance()
-					.getTransactionManagerGlue()
+			coordinator = ctx.register(DURABLE_2PC_PROTOCOL,
+					EndpointReferenceFactory.getInstance()
+							.getEndpointReference(
+									ParticipantRPCEndpoint.PORT_TYPE,
+									ctx.getActivityId()
+											.toReferencePropertiesType()));
+			TransactionImporter importer = TransactionManagerGlueFactory
+					.getInstance().getTransactionManagerGlue()
 					.getTransactionImporter();
 			// TODO: implement timeout
-			localTx= importer.importExternalTransaction(xid, Integer.MAX_VALUE);
-		}
-		catch (Exception e) {
+			localTx = importer
+					.importExternalTransaction(xid, Integer.MAX_VALUE);
+		} catch (Exception e) {
 			if (localTx != null) {
 				try {
 					xaTerminator.forget(xid);
-				}
-				catch (Exception _e) {
+				} catch (Exception _e) {
 					_e.printStackTrace();
 				}
 			}
@@ -71,33 +73,31 @@ public class ImportedTransaction extends InterposedTransaction {
 		lock();
 		try {
 			switch (status) {
-				case Status.STATUS_ACTIVE :
-					break;
-				case Status.STATUS_PREPARED :
-					return Vote.VoteCommit;
-				case Status.STATUS_COMMITTED :
-					return Vote.VoteReadOnly;
-				case Status.STATUS_ROLLEDBACK :
-					return Vote.VoteRollback;
-				default :
-					throw new IllegalStateException();
+			case Status.STATUS_ACTIVE:
+				break;
+			case Status.STATUS_PREPARED:
+				return Vote.VoteCommit;
+			case Status.STATUS_COMMITTED:
+				return Vote.VoteReadOnly;
+			case Status.STATUS_ROLLEDBACK:
+				return Vote.VoteRollback;
+			default:
+				throw new IllegalStateException();
 			}
-			status= Status.STATUS_PREPARING;
+			status = Status.STATUS_PREPARING;
 			try {
 				if (xaTerminator.prepare(xid) == XAResource.XA_RDONLY) {
-					status= Status.STATUS_COMMITTED;
+					status = Status.STATUS_COMMITTED;
 					return Vote.VoteReadOnly;
 				}
-				status= Status.STATUS_PREPARED;
+				status = Status.STATUS_PREPARED;
 				return Vote.VoteCommit;
-			}
-			catch (XAException e) {
+			} catch (XAException e) {
 				//	TODO: do we need to do a rollback for jotm ???
-				status= Status.STATUS_ROLLEDBACK;
+				status = Status.STATUS_ROLLEDBACK;
 				return Vote.VoteRollback;
 			}
-		}
-		finally {
+		} finally {
 			unlock();
 		}
 	}
@@ -106,18 +106,17 @@ public class ImportedTransaction extends InterposedTransaction {
 		lock();
 		try {
 			switch (status) {
-				case Status.STATUS_PREPARED :
-					break;
-				case Status.STATUS_COMMITTED :
-					return;
-				default :
-					throw new IllegalStateException();
+			case Status.STATUS_PREPARED:
+				break;
+			case Status.STATUS_COMMITTED:
+				return;
+			default:
+				throw new IllegalStateException();
 			}
-			status= Status.STATUS_COMMITTING;
+			status = Status.STATUS_COMMITTING;
 			xaTerminator.commit(xid, false);
-			status= Status.STATUS_COMMITTED;
-		}
-		finally {
+			status = Status.STATUS_COMMITTED;
+		} finally {
 			unlock();
 		}
 	}
@@ -126,20 +125,26 @@ public class ImportedTransaction extends InterposedTransaction {
 		lock();
 		try {
 			switch (status) {
-				case Status.STATUS_ACTIVE :
-				case Status.STATUS_PREPARED :
-					break;
-				case Status.STATUS_ROLLEDBACK :
-					return;
-				default :
-					throw new IllegalStateException();
+			case Status.STATUS_ACTIVE:
+			case Status.STATUS_PREPARED:
+				break;
+			case Status.STATUS_ROLLEDBACK:
+				return;
+			default:
+				throw new IllegalStateException();
 			}
-			status= Status.STATUS_ROLLING_BACK;
+			status = Status.STATUS_ROLLING_BACK;
 			xaTerminator.rollback(xid);
-			status= Status.STATUS_ROLLEDBACK;
-		}
-		finally {
+			status = Status.STATUS_ROLLEDBACK;
+		} finally {
 			unlock();
 		}
+	}
+
+	/**
+	 * @return Returns the coordinator.
+	 */
+	public EndpointReference getCoordinator() {
+		return coordinator;
 	}
 }
