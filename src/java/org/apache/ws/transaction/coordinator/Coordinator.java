a5e898f2a15f7313d7a7145db5bfa62a864b4062
Dasarath Weeratunge <dasarath@apache.org>
2005-12-24 02:06:14 +0000

    git-svn-id: https://svn.apache.org/repos/asf/webservices/kandula/branches/Kandula_1@358903 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/ws/transaction/coordinator/Coordinator.java
+++ src/java/org/apache/ws/transaction/coordinator/Coordinator.java
@@ -26,5 +26,5 @@ import org.apache.ws.transaction.wscoor._CoordinationContext;
 public interface Coordinator {
 	Identifier getActivityId();
 	_CoordinationContext getCoordinationContext();
-	EndpointReference registerParticipant(URI protocol, EndpointReference participant);
+	EndpointReference registerParticipant(URI protocol, EndpointReference participant) throws InvalidCoordinationProtocolException;
 }
