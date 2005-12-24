8f3a3198eb38193b82ee0ecbca91215f7d1b3305
Dasarath Weeratunge <dasarath@apache.org>
2005-12-24 02:07:51 +0000

    git-svn-id: https://svn.apache.org/repos/asf/webservices/kandula/branches/Kandula_1@358908 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/ws/transaction/wsat/ParticipantPort.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright  2004 The Apache Software Foundation.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package org.apache.ws.transaction.wsat;
+
+import java.net.URL;
+import java.rmi.RemoteException;
+
+import org.apache.ws.transaction.utility.Service;
+
+
+/**
+ * @author Publishing
+ * 
+ * Created on Jun 14, 2004
+ */
+public class ParticipantPort {
+	ParticipantPortType stub;
+
+	public ParticipantPort(org.apache.axis.message.addressing.EndpointReference epr) {
+		try {
+			stub= new ParticipantStub(new URL(epr.getAddress().toString()), new Service(epr));
+		}
+		catch (Exception e) {
+			throw new IllegalArgumentException(e.toString());
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.ws.transaction.wsat.ParticipantPortType#prepareOperation(org.apache.ws.transaction.wsat.Notification)
+	 */
+	public void prepareOperation() throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.ws.transaction.wsat.ParticipantPortType#commitOperation(org.apache.ws.transaction.wsat.Notification)
+	 */
+	public void commitOperation() throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.ws.transaction.wsat.ParticipantPortType#rollbackOperation(org.apache.ws.transaction.wsat.Notification)
+	 */
+	public void rollbackOperation() throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+}
