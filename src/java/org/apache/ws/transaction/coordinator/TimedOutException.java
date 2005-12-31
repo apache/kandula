964f7d7a3256fe3621d3aca434ea33debe56d8dd
Dasarath Weeratunge <dasarath@apache.org>
2005-12-31 04:05:39 +0000

    git-svn-id: https://svn.apache.org/repos/asf/webservices/kandula/branches/Kandula_1@360237 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/ws/transaction/coordinator/TimedOutException.java
@@ -0,0 +1,13 @@
+/*
+ * Created on Dec 30, 2005
+ *
+ */
+package org.apache.ws.transaction.coordinator;
+
+/**
+ * @author Dasarath Weeratunge
+ *
+ */
+public class TimedOutException extends RuntimeException {
+
+}
