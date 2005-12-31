2d2f7f479c406aef8a8fd89eb40ae43ad9ffd091
Dasarath Weeratunge <dasarath@apache.org>
2005-12-31 03:57:08 +0000

    git-svn-id: https://svn.apache.org/repos/asf/webservices/kandula/branches/Kandula_1@360229 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/ws/transaction/coordinator/at/BasicParticipant.java
@@ -0,0 +1,202 @@
+/*
+ * Created on Dec 30, 2005
+ *
+ */
+package org.apache.ws.transaction.coordinator.at;
+
+import java.rmi.RemoteException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.transaction.xa.XAException;
+import javax.transaction.xa.XAResource;
+
+import org.apache.axis.message.addressing.EndpointReference;
+import org.apache.ws.transaction.coordinator.CoordinationContext;
+import org.apache.ws.transaction.coordinator.ParticipantService;
+import org.apache.ws.transaction.coordinator.TimedOutException;
+import org.apache.ws.transaction.utility.Callback;
+import org.apache.ws.transaction.wsat.CoordinatorPortType;
+import org.apache.ws.transaction.wsat.Notification;
+import org.apache.ws.transaction.wsat.ParticipantPortType;
+import org.apache.ws.transaction.wscoor.Expires;
+
+/**
+ * @author Dasarath Weeratunge
+ *  
+ */
+public abstract class BasicParticipant implements ParticipantPortType, Callback {
+	private static Timer timer = new Timer();
+
+	public static final int RETRY_DELAY_MILLIS = 10 * 1000;
+
+	private EndpointReference c;
+
+	protected BasicParticipant(boolean durable, CoordinationContext ctx)
+			throws RemoteException {
+		long timeout = 0;
+		Expires ex = ctx.getExpires();
+		if (ex != null)
+			timeout = ex.get_value().longValue();
+		EndpointReference epr = ParticipantService.getInstance().getParticipantService(
+			this, timeout);
+		c = ctx.register(durable ? ATCoordinator.PROTOCOL_ID_DURABLE_2PC
+				: ATCoordinator.PROTOCOL_ID_VOLATILE_2PC, epr);
+	}
+
+	protected CoordinatorPortType getCoordinator() {
+		try {
+			return new CoordinatorStub(c);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected abstract int prepare() throws XAException;
+
+	protected abstract void commit() throws XAException;
+
+	protected abstract void rollback() throws XAException;
+
+	protected abstract void forget();
+
+	protected abstract int getStatus();
+
+	public synchronized void prepareOperation(Notification parameters)
+			throws RemoteException {
+		switch (getStatus()) {
+		case AT2PCStatus.NONE:
+			getCoordinator().abortedOperation(null);
+			return;
+
+		case AT2PCStatus.ACTIVE:
+			final CoordinatorPortType p = getCoordinator();
+			try {
+				if (prepare() == XAResource.XA_RDONLY) {
+					forget();
+					p.readOnlyOperation(null);
+				} else {
+					p.preparedOperation(null);
+					timer.schedule(new TimerTask() {
+						public void run() {
+							switch (getStatus()) {
+							case AT2PCStatus.NONE:
+							case AT2PCStatus.ACTIVE:
+							case AT2PCStatus.PREPARING:
+							case AT2PCStatus.ABORTING:
+							case AT2PCStatus.COMMITTING:
+								cancel();
+								return;
+
+							case AT2PCStatus.PREPARED:
+								try {
+									p.preparedOperation(null);
+								} catch (RemoteException e) {
+									e.printStackTrace();
+								}
+							}
+						}
+					}, RETRY_DELAY_MILLIS, RETRY_DELAY_MILLIS);
+				}
+			} catch (XAException e) {
+				forget();
+				p.abortedOperation(null);
+			}
+			return;
+
+		case AT2PCStatus.PREPARING:
+			return;
+
+		case AT2PCStatus.PREPARED:
+			getCoordinator().preparedOperation(null);
+			return;
+
+		case AT2PCStatus.ABORTING:
+			forget();
+			getCoordinator().abortedOperation(null);
+			return;
+
+		case AT2PCStatus.COMMITTING:
+		}
+	}
+
+	public synchronized void commitOperation(Notification parameters)
+			throws RemoteException {
+		switch (getStatus()) {
+		case AT2PCStatus.NONE:
+			getCoordinator().committedOperation(null);
+			return;
+
+		case AT2PCStatus.ACTIVE:
+		case AT2PCStatus.PREPARING:
+			try {
+				rollback();
+				forget();
+				getCoordinator().abortedOperation(null);
+			} catch (XAException e) {
+				e.printStackTrace();
+			}
+			return;
+
+		case AT2PCStatus.PREPARED:
+			try {
+				commit();
+				forget();
+				getCoordinator().committedOperation(null);
+			} catch (XAException e) {
+				e.printStackTrace();
+			}
+			return;
+
+		case AT2PCStatus.ABORTING:
+			throw new IllegalStateException();
+
+		case AT2PCStatus.COMMITTING:
+		}
+	}
+
+	public synchronized void rollbackOperation(Notification parameters)
+			throws RemoteException {
+		switch (getStatus()) {
+		case AT2PCStatus.NONE:
+			getCoordinator().abortedOperation(null);
+			return;
+
+		case AT2PCStatus.ACTIVE:
+		case AT2PCStatus.PREPARING:
+		case AT2PCStatus.PREPARED:
+			try {
+				rollback();
+				forget();
+				getCoordinator().abortedOperation(null);
+			} catch (XAException e) {
+				e.printStackTrace();
+			}
+			return;
+
+		case AT2PCStatus.ABORTING:
+			forget();
+			getCoordinator().abortedOperation(null);
+			return;
+
+		case AT2PCStatus.COMMITTING:
+			throw new IllegalStateException();
+		}
+	}
+
+	public void timeout() {
+		System.out.println("[BasicParticipant] timeout "
+				+ AT2PCStatus.getStatusName(getStatus()));
+		if (getStatus() == AT2PCStatus.NONE)
+			return;
+		try {
+			rollback();
+			forget();
+			getCoordinator().abortedOperation(null);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		throw new TimedOutException();
+	}
+}
