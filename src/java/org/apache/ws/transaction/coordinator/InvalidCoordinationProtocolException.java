f1297abe771ee1732d238d089d835180f92c5bea
Dasarath Weeratunge <dasarath@apache.org>
2006-01-07 05:35:27 +0000

    git-svn-id: https://svn.apache.org/repos/asf/webservices/kandula/branches/Kandula_1@366674 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/ws/transaction/coordinator/InvalidCoordinationProtocolException.java
@@ -0,0 +1,13 @@
+/*
+ * Created on Jan 6, 2006
+ *
+ */
+package org.apache.ws.transaction.coordinator;
+
+/**
+ * @author Dasarath Weeratunge
+ *
+ */
+public class InvalidCoordinationProtocolException extends Exception {
+
+}
