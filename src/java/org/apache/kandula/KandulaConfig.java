a4390998151c623182fc9c95839bbec5d7e4cfc4
Dasarath Weeratunge <dasarath@apache.org>
2007-05-25 14:45:34 +0000

    The patch changes the kandula.properties contents to include two URLs:
    * kandula.localService=http://localhost:8181/axis/services/
    This is the address Kandula will tell any partner when handing out references.
    
    * kandula.preferredCoordinationService=http://localhost:8281/axis/services/
    This is the address where Kandula will try to reach a Kandula 
    activiation service to create new transaction contexts.
    
    Hannes Erven, Georg Hicker
    
    git-svn-id: https://svn.apache.org/repos/asf/webservices/kandula/branches/Kandula_1@541674 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/kandula/KandulaConfig.java
+++ src/java/org/apache/kandula/KandulaConfig.java
@@ -1,6 +1,19 @@
 /*
- * Created on Jan 6, 2006
+ * Copyright 2007 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  *
+ *  @author Dasarath Weeratunge, Hannes Erven, Georg Hicker
  */
 package org.apache.kandula;
 
@@ -9,21 +22,23 @@ import java.io.InputStream;
 import java.util.Properties;
 
 /**
- * @author Dasarath Weeratunge
+ * @author Dasarath Weeratunge, Hannes Erven, Georg Hicker
  *  
  */
 public class KandulaConfig {
 
 	private static final String PROPERTY_FILE = "kandula.properties";
 
-	private static final String CONTEXT_PROPERTY = "kandula.context";
+	private static final String LOCAL_SERVICE__PROPERTY = "kandula.localService";
+
+	private static final String PREFERRED_SERVICE__PROPERTY = "kandula.preferredCoordinationService";
 
 	private static KandulaConfig instance = new KandulaConfig();
 
 	private Properties properties = null;
 
 	private KandulaConfig() {
-		properties = new Properties();
+		this.properties = new Properties();
 		loadProperties();
 	}
 
@@ -32,11 +47,10 @@ public class KandulaConfig {
 	}
 
 	private void loadProperties() {
-		InputStream in = getClass().getClassLoader().getResourceAsStream(
-			PROPERTY_FILE);
+		InputStream in = getClass().getClassLoader().getResourceAsStream(PROPERTY_FILE);
 
 		try {
-			properties.load(in);
+			this.properties.load(in);
 			in.close();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -45,7 +59,29 @@ public class KandulaConfig {
 	}
 
 	public String getContext() {
-		return properties.getProperty(CONTEXT_PROPERTY);
+		return this.properties.getProperty(LOCAL_SERVICE__PROPERTY);
 	}
 
+	/**
+	 * Return the configured local kandula services endpoint base URL, eg.
+	 * http://test1.kandula.apache.org:8280/axis/services/
+	 * @return String-URL
+	 */
+	public String getLocalServicesURL() {
+		return this.properties.getProperty(LOCAL_SERVICE__PROPERTY);
+	}
+
+	/**
+	 * Return the configured preferred kandula coordination services endpoint base URL, eg.
+	 * http://my-favorite-coordinator.bar.foo.org:8180/axis/services/
+	 * @return String-URL
+	 */
+	public String getKandulaServicesURL(){
+		final String externalURL = this.properties.getProperty(PREFERRED_SERVICE__PROPERTY);
+
+		if (externalURL != null && externalURL.length()>0)
+			return externalURL;
+		
+		return getContext();
+	}
 }
